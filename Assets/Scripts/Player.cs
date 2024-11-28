1c7cdd765af867dd94af85ff432ebbbbd718c11c
Arthur Djiomou <arthurdjiomou711@gmail.com>
2024-11-28 18:57:22 +0100

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,9 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
 
     public float speed = 10f;
-
     private float mov_x, mov_y;
+
+    private Vector2 mousePos;
 
     void Start()
     {
@@ -17,6 +18,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         GetInput();
+        CharacterRotate();
     }
 
     void FixedUpdate()
@@ -29,5 +31,13 @@ public class Player : MonoBehaviour
         mov_x = Input.GetAxisRaw("Horizontal");
         mov_y = Input.GetAxisRaw("Vertical");
 
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void CharacterRotate()
+    {
+        float angle = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x) * Mathf.Rad2Deg - 90f;
+    
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
