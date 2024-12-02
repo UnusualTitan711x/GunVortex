45ff0461595ecfe09ea176f9516f775803a86b8c
Arthur Djiomou <arthurdjiomou711@gmail.com>
2024-12-02 15:40:48 +0100

    inventory updates and fix

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -22,10 +22,16 @@ public class PlayerInventory : MonoBehaviour
         {
             EquipWeapon(1);
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            DropWeapon();
+        }
     }
 
     public void AddWeapon(WeaponData data) // Add field for current ammo later
     {
+
         GameObject newWeapon = Instantiate(data.equipPrefab, PlayerManager.instance.weaponHolder);
         newWeapon.GetComponent<Weapon>().weaponData = data;
         newWeapon.SetActive(false);
@@ -35,10 +41,9 @@ public class PlayerInventory : MonoBehaviour
 
     public void EquipWeapon(int index)
     {
-        if (index < 0 || index > 2)
-        {
-            return;
-        }
+        if (index < 0 || index > 2) return;
+        
+        if (index >= weapons.Count) return;
 
         if (currentWeapon != null)
         {
@@ -49,8 +54,21 @@ public class PlayerInventory : MonoBehaviour
         currentWeapon.SetActive(true);
     }
 
-    public void DropWeapon()
+    public void DropWeapon() // Don't forget to put ammo too
     {
-        
+        if (currentWeapon == null) return;
+
+        // Remove the current weapon from the inventory
+        weapons.Remove(currentWeapon);
+
+        // Spawn the weapon pickup in the world
+        GameObject worldPickup = Instantiate(currentWeapon.GetComponent<Weapon>().weaponData.pickupPrefab, PlayerManager.instance.weaponHolder.position, PlayerManager.instance.weaponHolder.rotation, null);
+        worldPickup.transform.SetParent(null);
+        weapons.Remove(currentWeapon);
+        Destroy(currentWeapon);
+        currentWeapon = null;
+
+
+        //If there is no space, swap the weapon with the current weapon
     }
 }
