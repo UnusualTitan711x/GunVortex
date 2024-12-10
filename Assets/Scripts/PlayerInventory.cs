884cb9528817cf08c64e6739048f68365a566807
Arthur Djiomou <arthurdjiomou711@gmail.com>
2024-12-10 18:25:51 +0100

    Test new weapon and bug fixes

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    // List that contains the weapons
     public List<GameObject> weapons = new List<GameObject>();
     public GameObject currentWeapon;
 
-    // Bullets
+    // Where the bullets are stored
     public int smallAmmo = 0;
     public int mediumAmmo = 0;
     public int shotgunShells = 0;
@@ -18,6 +19,7 @@ public class PlayerInventory : MonoBehaviour
 
     void Update()
     {
+        // Switch weapons using the 1 and 2 keys
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             EquipWeapon(0);
@@ -28,6 +30,7 @@ public class PlayerInventory : MonoBehaviour
             EquipWeapon(1);
         }
 
+        // Drop the weapon using the Q key
         if (Input.GetKeyDown(KeyCode.Q))
         {
             DropWeapon();
@@ -36,32 +39,37 @@ public class PlayerInventory : MonoBehaviour
 
     public void AddWeapon(WeaponData data) // Add field for current ammo later
     {
+        // Swap weapon if the space for weapon is full
         if (currentWeapon != null && weapons.Count >= 2)
         {
             SwapWeapon(data);
             return;
         }
 
+        // Put the weapon in the player's hands
         GameObject newWeapon = Instantiate(data.equipPrefab, PlayerManager.instance.weaponHolder);
         newWeapon.GetComponent<Weapon>().weaponData = data;
         
+        // Add the weapon to the inventory if there is already a weapon in the inventory.
+        // Else, the weapon added is equipped
         if (weapons.Count >= 1) newWeapon.SetActive(false);
         if (weapons.Count == 0) currentWeapon = newWeapon;
 
         weapons.Add(newWeapon);
-        
-        
     }
 
     public void EquipWeapon(int index)
-    {        
+    {
+        // Avoid going out of bounds of the weapons list
         if (index < 0 || index >= weapons.Count) return;
 
+        // Deactivate the current weapon if it is active
         if (currentWeapon != null)
         {
             currentWeapon.SetActive(false);
         }
 
+        // Set the other weapon active
         currentWeapon = weapons[index];
         currentWeapon.SetActive(true);
     }
@@ -74,22 +82,30 @@ public class PlayerInventory : MonoBehaviour
         weapons.Remove(currentWeapon);
 
         // Spawn the weapon pickup in the world
-        GameObject worldPickup = Instantiate(currentWeapon.GetComponent<Weapon>().weaponData.pickupPrefab, PlayerManager.instance.weaponHolder.position, PlayerManager.instance.weaponHolder.rotation, null);
+        GameObject worldPickup = Instantiate(currentWeapon.GetComponent<Weapon>().weaponData.pickupPrefab, PlayerManager.instance.weaponHolder.position, Quaternion.Euler(Vector3.zero), null);
         worldPickup.transform.SetParent(null);
+
+        // Remove weapon from hand and inventory
         Destroy(currentWeapon);
         currentWeapon = null;
+
+        // Equip the first weapon when the other has been dropped
+        if (weapons.Count != 0) EquipWeapon(0); 
     }
 
     public void SwapWeapon(WeaponData data)
     {
+        // Remove weapon from inventory
         DropWeapon();
 
+        // Add the other weapon to inventory and into the player's hands
         GameObject newWeapon = Instantiate(data.equipPrefab, PlayerManager.instance.weaponHolder);
         newWeapon.GetComponent<Weapon>().weaponData = data;
         weapons.Add(newWeapon);
         EquipWeapon(1);
     }
 
+    // Sets the bullet that the weapon should use from the inventory
     public int SelectAmmo(BulletType type)
     {
         if (type == BulletType.small) return smallAmmo;
